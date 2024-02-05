328cec688579e63019d648fe199592f7bf60ef71
shady <shadyrajaab@gmail.com>
2024-02-05 11:54:25 -0300

    Corrigindo a os parâmetros do .connect

--- database/connection.py
+++ database/connection.py
@@ -2,19 +2,21 @@ import psycopg2
 
 class DataBase:
     def __init__(self, host, database, user, password):
-        self.host = host
-        self.database = database
-        self.user = user
-        self.password = password
-
-        self.cursor = self.__create_connection__()
+        self.cursor = self.__create_connection__(
+            host, database, user, password
+        )
     
-    def __create_connection__(self):
+    def __create_connection__(self, host, database, user, password):
         """
             Realizando a conexão com o banco de dados da Freecel.
         """
         
-        connection = psycopg2.connect(self.host, self.database, self.user, self.password)
+        connection = psycopg2.connect(
+            host = host, 
+            database = database, 
+            user = user, 
+            password = password
+        )
         cursor = connection.cursor()
 
         return cursor
