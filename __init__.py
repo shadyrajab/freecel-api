890eed1c6305f6c9db6311440927d0ffb9eaddf3
shady <shadyrajaab@gmail.com>
2024-02-08 11:07:42 -0300

    Deletando __init__

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from .client.client import Freecel
-
-# Especifique quais símbolos serão exportados quando alguém fizer "from seu_modulo import *"
-__all__ = [
-    "Freecel"
-    # Adicione outros símbolos conforme necessário
-]
