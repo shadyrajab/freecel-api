0b39adf7b71140817465ff648e13c9d98fe42918
shady <shadyrajaab@gmail.com>
2024-02-29 13:33:55 -0300

    feat: adicionar pasta e arquivo utils

---
+++ utils/variables.py
@@ -0,0 +1,6 @@
+TIPO_VENDA = {"FIXA", "AVANÇADA", "MIGRAÇÃO PRÉ-PÓS", "VVN", "ALTAS"}
+EQUIPE = {"FREECEL", "VALPARAISO", "PARCEIRO", "ESCRITORIO"}
+UF = {
+    "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT","MS", "MG", "TO"
+    "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE"
+}
