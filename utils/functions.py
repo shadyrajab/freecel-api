3471d9888fa3adfc0c4f20225e5398ad0dc0bb7c
shady <shadyrajaab@gmail.com>
2024-03-04 16:28:25 -0300

    feat: remover funções desnecessárias

--- utils/functions.py
+++ utils/functions.py
@@ -1,8 +1,6 @@
 from pandas import DataFrame
 from json import load
 from io import StringIO
-from client.client import Client
-from utils.variables import HOST, DATABASE, USER, PASSWORD
     
 def group_by(dataframe: DataFrame, column: str, sort: str) -> DataFrame:
     grouped_dataframe = dataframe.groupby(column, as_index = False).sum(
@@ -14,11 +12,3 @@ def group_by(dataframe: DataFrame, column: str, sort: str) -> DataFrame:
 def jsonfy(dataframe: DataFrame):
     df = dataframe.to_json(orient = 'records')
     return load(StringIO(df))
-
-def create_client() -> Client:
-    return Client(
-        host = HOST,
-        database = DATABASE,
-        user = USER,
-        password = PASSWORD
-    )
