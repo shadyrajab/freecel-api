67ee2f0e0eeaf421450d5af9e90b23050071df45
shady <shadyrajaab@gmail.com>
2024-03-05 15:11:07 -0300

    feat: adicionar sistema adabas

--- utils/functions.py
+++ utils/functions.py
@@ -1,6 +1,7 @@
 from pandas import DataFrame
 from json import load
 from io import StringIO
+from utils.variables import adabas_mapping
     
 def group_by(dataframe: DataFrame, column: str, sort: str) -> DataFrame:
     grouped_dataframe = dataframe.groupby(column, as_index = False).sum(
@@ -12,3 +13,7 @@ def group_by(dataframe: DataFrame, column: str, sort: str) -> DataFrame:
 def jsonfy(dataframe: DataFrame):
     df = dataframe.to_json(orient = 'records')
     return load(StringIO(df))
+
+def get_adabas(equipe, tipo):
+    return adabas_mapping.get((equipe, tipo), None)
+
