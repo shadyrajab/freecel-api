0116a57e50db796caa14a2c42a37fc3b283ea4a1
shady <shadyrajaab@gmail.com>
2024-03-08 09:15:19 -0300

    fix: alterar porta

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ for route in routes:
     app.include_router(route)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
