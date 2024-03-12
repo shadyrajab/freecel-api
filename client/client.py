286cfdb15a4c5f7827eb324cd532c831fa2d73c9
shady <shadyrajaab@gmail.com>
2024-03-12 15:10:58 -0300

    refactor: mudar métodos para async

--- client/client.py
+++ client/client.py
@@ -9,26 +9,27 @@ import pandas as pd
 class Client(DataBase):
     async def __aenter__(self):
         await super().__aenter__()
-        self.dataframe = await self.get_vendas(to_dataframe=True)
-        self.__format()
-
         return self
     
     async def __aexit__(self, exc_type, exc, tb):
         await super().__aexit__(exc_type, exc, tb)
          
-    def Consultor(self, nome: str, ano: Optional[int] = None, mes: Optional[str] = None, jsonfy: Optional[bool] = None, display_vendas: Optional[bool] = None) -> Consultor:
-        return Consultor(self.dataframe[self.dataframe['consultor'] == nome], ano, mes, jsonfy, display_vendas)
+    async def Consultor(self, nome: str, ano: Optional[int] = None, mes: Optional[str] = None, jsonfy: Optional[bool] = None, display_vendas: Optional[bool] = None) -> Consultor:
+        dataframe = self.__format(await self.get_vendas(to_dataframe=True))
+        return Consultor(dataframe[dataframe['consultor'] == nome], ano, mes, jsonfy, display_vendas)
     
-    def Ranking(self, ano: Optional[int] = None, mes: Optional[str] = None, jsonfy: Optional[bool] = None) -> Rankings:
-        return Rankings(self.dataframe, ano, mes, jsonfy)
+    async def Ranking(self, ano: Optional[int] = None, mes: Optional[str] = None, jsonfy: Optional[bool] = None) -> Rankings:
+        dataframe = self.__format(await self.get_vendas(to_dataframe=True))
+        return Rankings(dataframe, ano, mes, jsonfy)
     
-    def Freecel(self, ano: Optional[int] = None, mes: Optional[str] = None, jsonfy: Optional[bool] = None) -> Freecel:
-        return Freecel(self.dataframe, ano, mes, jsonfy, True)
+    async def Freecel(self, ano: Optional[int] = None, mes: Optional[str] = None, jsonfy: Optional[bool] = None) -> Freecel:
+        dataframe = self.__format(await self.get_vendas(to_dataframe=True))
+        return Freecel(dataframe, ano, mes, jsonfy, True)
     
     # Essas 3 funções abaixo estão muito mal escritas
     async def vendas(self, ano: Optional[int] = None, mes: Optional[str] = None, as_json: Optional[bool] = None):
-        dataframe = filter_by(self.dataframe, ano, mes)
+        dataframe = self.__format(await self.get_vendas(to_dataframe=True))
+        dataframe = filter_by(dataframe, ano, mes)
         if as_json:
             return jsonfy(dataframe)
         
@@ -48,9 +49,11 @@ class Client(DataBase):
         
         return consultores
 
-    def __format(self):
-        self.dataframe['ano'] = self.dataframe['data'].dt.year
-        self.dataframe['mês'] = self.dataframe['data'].dt.month.apply(lambda mes: get_mes(mes))
-        self.dataframe[['valor_acumulado', 'valor_do_plano', 'quantidade_de_produtos']] = self.dataframe[
+    def __format(self, dataframe: pd.DataFrame):
+        dataframe['ano'] = dataframe['data'].dt.year
+        dataframe['mês'] = dataframe['data'].dt.month.apply(lambda mes: get_mes(mes))
+        dataframe[['valor_acumulado', 'valor_do_plano', 'quantidade_de_produtos']] = dataframe[
                 ['valor_acumulado', 'valor_do_plano', 'quantidade_de_produtos']
             ].apply(pd.to_numeric, errors='coerce', downcast='integer')
+        
+        return dataframe
