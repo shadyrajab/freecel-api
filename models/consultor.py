045a3541229c41b95f852a2bc39bcff0dc25cf0c
shady <shadyrajaab@gmail.com>
2024-03-13 17:12:54 -0300

    feat: adicionar novos campos model

--- models/consultor.py
+++ models/consultor.py
@@ -1,5 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+
+from utils.variables import CARGOS, EQUIPE
 
 
 class Vendedor(BaseModel):
     name: str
+    vinculo: str
+    cargo: str
+
+    @validator("vinculo")
+    def validate_vinculo(cls, value):
+        if value not in EQUIPE:
+            raise ValueError(f"A equipe {value} não existe")
+
+        return value.upper()
+
+    @validator("cargo")
+    def validate_cargo(cls, value):
+        if value not in CARGOS:
+            raise ValueError(f"O cargo {value} não existe")
+
+        return value.upper()
