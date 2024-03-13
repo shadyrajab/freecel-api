d0a507fbe5356ce9d4df391263b17501785aef15
shady <shadyrajaab@gmail.com>
2024-03-13 15:10:16 -0300

    feat: adicionar models para request body

---
+++ params/request_body.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UpdateProdutoParams(BaseModel):
+    id: int = Field(description="ID do Produto")
+    nome: Optional[str] = Field(None, description="Nome do Produto")
+    preco: Optional[float] = Field(None, description="Preço do produto")
+
+
+class UpdateVendaParams(BaseModel):
+    id: int = Field(description="ID do Produto")
+    cnpj: Optional[str] = Field(None, description="CNPJ do Cliente")
+    consultor: Optional[str] = Field(None, description="Nome do Consultor")
+    telefone: Optional[str] = Field(None, description="Telefone do Cliente")
+    plano: Optional[str] = Field(None, description="Plano Comprado")
+    gestor: Optional[str] = Field(None, description="Nome do Gestor")
+    tipo: Optional[str] = Field(None, description="Tipo da Venda")
+    status: Optional[str] = Field(None, description="Status da Venda")
+    email: Optional[EmailStr] = Field(None, description="Email do Cliente")
+    ja_cliente: Optional[bool] = Field(None, description="Já Cliente?")
+    numero_pedido: Optional[str] = Field(None, description="Número do Pedido")
+
+
+class UpdateConsultorParams(BaseModel):
+    id: int = Field(description="ID do Consultor")
+    nome: Optional[str] = Field(None, description="Nome do Consultor")
+    vinculo: Optional[str] = Field(None, description="Tipo de Vínculo")
+    cargo: Optional[str] = Field(None, description="Cargo do Consultor")
