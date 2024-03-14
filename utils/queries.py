95e8deeaaea92b16342ef63412b37f6cb96d2b92
shady <shadyrajaab@gmail.com>
2024-03-14 13:31:46 -0300

    refactor: reescrever queries

--- utils/queries.py
+++ utils/queries.py
@@ -1,21 +1,47 @@
-ADD_VENDA_QUERY = """
-    INSERT INTO vendas_concluidas (
-        cnpj, telefone, consultor, data, gestor, plano, quantidade_de_produtos, 
-        revenda, tipo, uf, valor_acumulado, valor_do_plano, email, quadro_funcionarios,
-        faturamento, cnae, cep, municipio, porte, capital_social, natureza_juridica,
-        matriz, regime_tributario, bairro, adabas, ja_cliente
-    ) VALUES (
-        %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-    )
-"""
+ADD_VENDA_LIST_PARAMS = [
+    "cnpj",
+    "telefone",
+    "consultor",
+    "data",
+    "gestor",
+    "plano",
+    "volume",
+    "equipe",
+    "tipo",
+    "ddd",
+    "uf",
+    "receita",
+    "preco",
+    "email",
+    "quadro_funcionarios",
+    "faturamento",
+    "cnae",
+    "cep",
+    "municipio",
+    "porte",
+    "capital_social",
+    "natureza_juridica",
+    "matriz",
+    "regime_tributario",
+    "bairro",
+    "adabas",
+    "ja_cliente",
+    "numero_pedido",
+    "status",
+]
 
+placeholders = ", ".join(["$" + str(i + 1) for i in range(len(ADD_VENDA_LIST_PARAMS))])
+columns = ", ".join(ADD_VENDA_LIST_PARAMS)
+ADD_VENDA_QUERY = f"INSERT INTO vendas_concluidas ({columns}) VALUES ({placeholders})"
 REMOVE_VENDA_QUERY = "DELETE FROM vendas_concluidas WHERE id = ($1)"
 GET_VENDAS_QUERY = "SELECT * FROM vendas_concluidas"
-REMOVE_PRODUTO_QUERY = "DELETE FROM produtos WHERE id = (%s)"
-ADD_PRODUTO_QUERY = "INSERT INTO produtos (nome, preco) VALUES (%s, %s)"
+REMOVE_PRODUTO_QUERY = "DELETE FROM produtos WHERE id = ($1)"
+ADD_PRODUTO_QUERY = "INSERT INTO produtos (nome, preco) VALUES ($1, $2)"
 GET_PRODUTOS_QUERY = "SELECT * FROM produtos"
-REMOVE_CONSULTOR_QUERY = "DELETE FROM consultores WHERE id = (%s)"
-ADD_CONSULTOR_QUERY = "INSERT INTO consultores (nome, vinculo, cargo) VALUES ($1, $2, $3)"
+REMOVE_CONSULTOR_QUERY = "DELETE FROM consultores WHERE id = ($1)"
+ADD_CONSULTOR_QUERY = (
+    "INSERT INTO consultores (nome, vinculo, cargo) VALUES ($1, $2, $3)"
+)
 GET_CONSULTORES_QUERY = "SELECT * FROM consultores"
 JWT_QUERY = "SELECT * FROM uuids WHERE uuid = $1"
-GET_PRECO_QUERY = "SELECT preco FROM produtos WHERE nome = %s"
+GET_PRECO_QUERY = "SELECT preco FROM produtos WHERE nome = $1"
