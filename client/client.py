34b096ed63541f2ae7bedb9d3fa2af33d78a38d5
shady <shadyrajaab@gmail.com>
2024-03-15 11:50:30 -0300

    feat: adicionar tipagem

--- client/client.py
+++ client/client.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Union
 
 import pandas as pd
 
@@ -43,13 +43,15 @@ class Client(DataBase):
         self,
         ano: Optional[int] = None,
         mes: Optional[str] = None,
+        equipe: Optional[str] = None,
         jsonfy: Optional[bool] = None,
     ) -> Freecel:
         dataframe = self.__format(await self.get_vendas(to_dataframe=True))
-        return Freecel(dataframe, ano, mes, jsonfy, True)
+        return Freecel(dataframe, ano, mes, equipe, jsonfy, True)
 
-    # Essas 3 funções abaixo estão muito mal escritas
-    async def vendas(self, as_json: Optional[bool] = None, **filters: str):
+    async def vendas(
+        self, as_json: Optional[bool] = None, **filters: str
+    ) -> Union[list, pd.DataFrame]:
         dataframe = self.__format(await self.get_vendas(to_dataframe=True))
         dataframe = filter_by(dataframe, **filters)
         if as_json:
@@ -57,21 +59,25 @@ class Client(DataBase):
 
         return dataframe
 
-    async def produtos(self, as_json: Optional[bool] = None):
+    async def produtos(
+        self, as_json: Optional[bool] = None
+    ) -> Union[list, pd.DataFrame]:
         produtos = await self.get_produtos(to_dataframe=True)
         if as_json:
             return jsonfy(produtos)
 
         return produtos
 
-    async def consultores(self, as_json: Optional[bool] = None):
+    async def consultores(
+        self, as_json: Optional[bool] = None
+    ) -> Union[list, pd.DataFrame]:
         consultores = await self.get_consultores(to_dataframe=True)
         if as_json:
             return jsonfy(consultores)
 
         return consultores
 
-    def __format(self, dataframe: pd.DataFrame):
+    def __format(self, dataframe: pd.DataFrame) -> pd.DataFrame:
         dataframe["ano"] = dataframe["data"].dt.year
         dataframe["mes"] = dataframe["data"].dt.month.apply(lambda mes: get_mes(mes))
         dataframe[["receita", "preco", "volume"]] = dataframe[
