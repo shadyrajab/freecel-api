bdbd75b553051b9419c32e5e4bccff4eac11529d
shady <shadyrajaab@gmail.com>
2024-03-18 17:27:46 -0300

    feat: iniciar sistema de logging

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import logging
 import os
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.responses import JSONResponse
 
-from routes import consultor, produtos, rankings, stats, vendas, migracoes
+from routes import consultor, migracoes, produtos, rankings, stats, vendas
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    filename="logs/main.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - def %(funcName)s - %(message)s",
+)
 
 app = FastAPI()
 app.add_middleware(GZipMiddleware, minimum_size=1000)
@@ -15,11 +24,21 @@ routes = [
     rankings.router,
     stats.router,
     vendas.router,
-    migracoes.router
+    migracoes.router,
 ]
 
 for route in routes:
     app.include_router(route)
+
+
+@app.exception_handler(Exception)
+async def exception_handler(request: Request, exc: Exception):
+    logging.error(f"Ocorreu um erro na solicitação: {exc}")
+    return JSONResponse(
+        status_code=500,
+        content={"error": "Ocorreu um erro ao atender sua solicitação."},
+    )
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
--- routes/consultor.py
+++ routes/consultor.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import APIRouter, Depends, Query
 
 from authenticator.jwt import authenticate
@@ -6,47 +8,88 @@ from models.consultor import Vendedor
 from models.identify import ID
 from params.request_body import UpdateConsultorParams
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logging.basicConfig(
+    filename="logs/consultor.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(funcName)s - %(message)s",
+)
+
 router = APIRouter()
 
 
-@router.get("/consultores", dependencies=[Depends(authenticate)])
+async def handle_request(client_method, *args, **kwargs):
+    function = client_method.__name__
+    try:
+        result = await client_method(*args, **kwargs)
+        logging.info(f"{function} params {kwargs} by {args}")
+        if function == "Consultor":
+            return result.to_json()
+
+        if function in {"update_consultor", "remove_consultor"}:
+            logging.info(f"{function} params {kwargs}")
+            return {
+                "status_code": 200,
+                "message": f"Solicitação realizada com sucesso {function}",
+                "params": kwargs,
+            }
+
+        return result
+
+    except Exception as e:
+        logging.error(f"{function} params {kwargs} error: {e}")
+        return {
+            "status_code": 500,
+            "message": "Ocorreu um erro ao atender sua solicitação",
+            "params": kwargs,
+            "exception": str(e),
+        }
+
+
+@router.get("/consultores")
 async def consultores():
     async with Client() as client:
-        consultores = await client.consultores(True)
-        return consultores
+        return await handle_request(client.consultores, as_json=True)
 
 
-@router.post("/consultores", dependencies=[Depends(authenticate)])
-async def add_consultor(consultor: Vendedor):
+@router.post("/consultores")
+async def add_consultor(consultor: Vendedor, user: str = Depends(authenticate)):
     async with Client() as client:
-        await client.add_consultor(consultor)
-        return {"message": "Consultor adicionado com sucesso"}
+        return await handle_request(client.add_consultor, consultor)
 
 
-@router.delete("/consultores", dependencies=[Depends(authenticate)])
-async def remove_consultor(id: ID):
+@router.delete("/consultores")
+async def remove_consultor(id: ID, user: str = Depends(authenticate)):
     async with Client() as client:
-        await client.remove_consultor(id)
-        return {"message": "Consultor removido com sucesso"}
+        return await handle_request(client.remove_consultor, id)
 
 
-@router.put("/consultores", dependencies=[Depends(authenticate)])
-async def update_consultor(params: UpdateConsultorParams):
+@router.put("/consultores")
+async def update_consultor(
+    params: UpdateConsultorParams, user: str = Depends(authenticate)
+):
+    params_filtered = {
+        key: value for key, value in params.model_dump().items() if value is not None
+    }
     async with Client() as client:
-        await client.update_consultor(**params.model_dump())
-        return {"message": "Consultor atualizado com sucesso"}
+        return await handle_request(client.update_consultor, **params_filtered)
 
 
-@router.get("/consultores/{nome_consultor}", dependencies=[Depends(authenticate)])
+@router.get("/consultores/{nome_consultor}")
 async def consultor(
     nome_consultor: str,
     ano: int = Query(None, description="Ano"),
     mes: str = Query(None, description="Mês"),
-    display_vendas: bool = Query(None, description="Mostrar vendas"),
+    display_vendas: bool = Query(False, description="Mostrar vendas"),
 ):
     nome_consultor = nome_consultor.replace("_", " ").upper()
     async with Client() as client:
-        consultor = await client.Consultor(
-            nome_consultor, ano, mes, True, display_vendas
+        return await handle_request(
+            client.Consultor,
+            nome=nome_consultor,
+            ano=ano,
+            mes=mes,
+            jsonfy=True,
+            display_vendas=display_vendas,
         )
-        return consultor.to_json()
