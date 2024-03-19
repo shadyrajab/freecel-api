0453c26cd5691788ff07c58a32e2f1d6895d1d8d
shady <shadyrajaab@gmail.com>
2024-03-19 09:20:41 -0300

    refactor: adicionar handle request

--- routes/migracoes.py
+++ routes/migracoes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, Query
 
 from authenticator.jwt import authenticate
 from client.client import Client
+from handler.handler_request import handle_request
 from params.request_body import UpdateMigracaoParams
 
 router = APIRouter()
@@ -12,12 +13,17 @@ async def migracoes(
     ano: int = Query(None, description="Ano"), mes: str = Query(None, description="Mês")
 ):
     async with Client() as client:
-        migracoes = await client.vendas(as_json=True, ano=ano, mes=mes, tipo="MIGRAÇÃO")
-        return migracoes
+        return await handle_request(
+            client.vendas, as_json=True, ano=ano, mes=mes, tipo="MIGRAÇÃO"
+        )
 
 
 @router.put("/migracoes", dependencies=[Depends(authenticate)])
-async def update_migracao(params: UpdateMigracaoParams):
+async def update_migracao(
+    params: UpdateMigracaoParams, user: str = Depends(authenticate)
+):
+    params_filtered = {
+        key: value for key, value in params.model_dump().items() if value is not None
+    }
     async with Client() as client:
-        await client.update_venda(**params.model_dump())
-        return {"message": "Venda atualizada com sucesso"}
+        return await handle_request(client.update_venda, user, **params_filtered)
