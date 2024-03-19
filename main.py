2ca5cca1290d9b03f845320476c82dd9bba57eb1
shady <shadyrajaab@gmail.com>
2024-03-19 11:09:44 -0300

    feat: silenciar logs do uvicorn

--- main.py
+++ main.py
@@ -40,4 +40,9 @@ async def exception_handler(request: Request, exc: Exception):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        access_log=False
+    )
