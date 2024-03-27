cd35508578a8b9808a4e5887988d9e40c68f8747
shady <shadyrajaab@gmail.com>
2024-03-27 09:14:25 -0300

    refactor: remover docstrings

--- utils/functions.py
+++ utils/functions.py
@@ -7,7 +7,6 @@ from utils.variables import MONTHS, adabas_mapping
 
 
 def get_clause(**params):
-    """Cria os valores necessários para criar uma query SQL de acordo com os kwargs passados"""
     id = params.get("id", None)
     if id is None:
         return
@@ -27,7 +26,6 @@ def get_clause(**params):
 
 
 def group_by(dataframe: DataFrame, column: str, sort: str) -> DataFrame:
-    """Agrupa o DataFrame e ordena de acordo com a chave passada"""
     grouped_dataframe = (
         dataframe.groupby(column, as_index=False)
         .sum(numeric_only=True)
@@ -38,13 +36,11 @@ def group_by(dataframe: DataFrame, column: str, sort: str) -> DataFrame:
 
 
 def jsonfy(dataframe: DataFrame):
-    """Converte um pd.DataFrame em um JSON"""
     df = dataframe.to_json(orient="records")
     return load(StringIO(df))
 
 
 def get_adabas(equipe, tipo) -> str:
-    """Mapeia e retorna o ADABAS da venda de acordo com a Equipe e o Tipo de venda"""
     return adabas_mapping.get((equipe, tipo), None)
 
 
@@ -77,5 +73,4 @@ def filter_by(dataframe: DataFrame, **filters: str) -> DataFrame:
 
 
 def get_mes(mes) -> str:
-    """Função retorna o mês de acordo com o Index, feita para formatar as datas do DataFrame"""
     return MONTHS[mes - 1]
