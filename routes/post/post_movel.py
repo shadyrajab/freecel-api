a90ac43ce618ff52c8eeea64e9a8a2a3af7b053a
shady <shadyrajaab@gmail.com>
2024-05-02 14:52:44 -0300

    refactor: atualizar imports

--- routes/post/post_movel.py
+++ routes/post/post_movel.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 from authenticator.jwt import authenticate
 from client.client import Client
 from handler.handler_request import handle_request
-from models.requestmodels.vendas import Venda
+from models.requestmodels.abstract.vendas import Venda
 
 router = APIRouter(prefix="/vendas", tags=["Vendas"])
 
