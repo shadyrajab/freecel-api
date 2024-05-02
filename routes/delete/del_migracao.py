84c1b283ecbbd9499d7ec1eedc8cbe0f2c52301f
shady <shadyrajaab@gmail.com>
2024-05-02 17:02:02 -0300

    feat: adiciona rotas para a deleção de vendas

--- routes/delete/del_migracao.py
+++ routes/delete/del_migracao.py
@@ -5,10 +5,10 @@ from client.client import Client
 from handler.handler_request import handle_request
 from models.identify import ID
 
-router = APIRouter(prefix="/vendas/movel", tags=["vendas/movel"])
+router = APIRouter(prefix="/migracao", tags=["migracao"])
 
 
 @router.delete("/")
-async def remove_movel(id: ID, user: str = Depends(authenticate)):
+async def remove_migracao(id: ID, user: str = Depends(authenticate)):
     async with Client() as client:
-        return await handle_request(client.remove_venda_movel, user, **{"id": id})
+        return await handle_request(client.remove_migracao, user, **{"id": id})
