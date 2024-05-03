e35b18b83ac8f9e043208c842e57edd971a66dce
shady <shadyrajaab@gmail.com>
2024-05-03 09:34:49 -0300

    refactor: atualizar métodos

--- routes/put/put_fixa.py
+++ routes/put/put_fixa.py
@@ -3,17 +3,17 @@ from fastapi import APIRouter, Depends
 from authenticator.jwt import authenticate
 from client.client import Client
 from handler.handler_request import handle_request
-from params.request_body import UpdateMigracaoParams
+from params.request_body import UpdateVendaFixaParams
 
-router = APIRouter(prefix="/migracoes", tags=["migração"])
+router = APIRouter(prefix="/vendas/fixa", tags=["vendas/fixa"])
 
 
 @router.put("/")
-async def update_migracao(
-    params: UpdateMigracaoParams, user: str = Depends(authenticate)
+async def update_venda_fixa(
+    params: UpdateVendaFixaParams, user: str = Depends(authenticate)
 ):
     params_filtered = {
         key: value for key, value in params.model_dump().items() if value is not None
     }
     async with Client() as client:
-        return await handle_request(client.update_migracao, user, **params_filtered)
+        return await handle_request(client.update_venda_fixa, user, **params_filtered)
