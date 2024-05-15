2fd60bc0d1912084158e36470e736e74453fb510
shady <shadyrajaab@gmail.com>
2024-05-15 10:01:32 -0300

    feat: adicionar novos rankings

--- structures/abstract/ranking.py
+++ structures/abstract/ranking.py
@@ -3,7 +3,6 @@ from typing import List, Optional
 import pandas as pd
 
 from utils.functions import jsonfy
-from utils.utils import SUPERVISORES
 
 
 class Rankings:
@@ -15,17 +14,33 @@ class Rankings:
         return self.get_ranking("plano")
 
     @property
-    def ranking_geral(self) -> pd.DataFrame:
+    def ranking_consultores(self) -> pd.DataFrame:
         return self.get_ranking("consultor")
+
+    @property
+    def ranking_vinculos(self) -> pd.DataFrame:
+        return self.get_ranking("vinculo")
+    
+    @property
+    def ranking_equipes(self) -> pd.DataFrame:
+        return self.get_ranking("equipe")
+
+    @property
+    def ranking_tipos(self) -> pd.DataFrame:
+        return self.get_ranking("tipo")
+    
+    @property
+    def ranking_status(self) -> pd.DataFrame:
+        return self.get_ranking("status")
 
     def get_ranking(self, column: str, tipo: Optional[List] = None) -> pd.DataFrame:
         dataframe = self.dataframe.copy()
+        print(dataframe.info())
         if tipo:
             dataframe = dataframe[dataframe["tipo"].isin(tipo)]
 
-        dataframe['receita'] = dataframe['receita'].astype(float)
-        ranking = dataframe.groupby(column, as_index=False).sum(numeric_only=True)[[column,'receita', 'volume']]
-        if column == "consultor":
-            ranking = ranking[~ranking["consultor"].isin(SUPERVISORES)]
-
+        dataframe["receita"] = dataframe["receita"].astype(float)
+        ranking = dataframe.groupby(column, as_index=False).sum(numeric_only=True)[
+            [column, "receita", "volume"]
+        ]
         return jsonfy(ranking)
